395eefb5980268f838719d41aa96704b6cc86b5b
unknown <robert@robertmauger.net>
2020-02-10 10:28:02 -0500

    cleanup

--- src/components/Memberships.vue
+++ src/components/Memberships.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="wrapper">
     <header>
-      <h1>Membership</h1>
-      <p>Select your membership type below:</p>
-      <div class="enumeration">
-        <span class="section-number">1</span>
-      </div>
+      <h1>Members</h1>
     </header>
 
     <div class="container">
@@ -13,18 +9,8 @@
       <div class="row">
         
           <div class="radio-block" v-for="memberships1 in memberships" :key="memberships1.id">
-            
-            {{memberships1.name}}<br>
-
-
-            
+            {{memberships1.name}}         
           </div>
-
-           <!-- <span v-if="memberships.icon.length<=1"> -->
-         
-       
-
-        <!-- v-if="Math.random() > 0.5" -->
       </div>
     </div>
   </section>
@@ -58,34 +44,13 @@ body {
 }
 
 header .enumeration {
-  text-align: center;
-  position: absolute;
-  top: 1px;
-  left: -58px;
+ 
   background: #1178ce;
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #1178ce;
-  height: 20px;
-  border-radius: 50%;
-  -moz-border-radius: 50%;
-  -webkit-border-radius: 50%;
-  width: 20px;
+  
 }
 
 header {
   position: relative;
 }
 
-.container {
-  display: flex;
-  /*     display:inline-block; */
-  justify-content: center;
-}
-.row {
-  display: flex;
-  justify-content: center;
-}
 </style>
